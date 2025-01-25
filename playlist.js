0fb65fae8478019695bf481285aa341a3d9c33e9
Sandro Dolidze <sdolidze3@gmail.com>
2025-01-25 18:00:09 +0000

    refactor: extract playlist into a separate file

---
+++ playlist.js
@@ -0,0 +1,296 @@
+const playlist = [
+    {
+        src: "tracks/track-1.mp3",
+        albumArt: "images/album1.jpg",
+        trackName: "Californication",
+        lyrics: `Psychic spies from China try to steal your mind's elation
+      And little girls from Sweden dream of silver screen quotation
+      And if you want these kind of dreams it's Californication
+      It's the edge of the world and all of Western civilization
+      The sun may rise in the East, at least it settled in a final location
+      It's understood that Hollywood sells Californication
+      Pay your surgeon very well to break the spell of aging
+      Celebrity skin, is this your chin, or is that war you're waging?
+      First born unicorn
+      Hardcore soft porn
+      Dream of Californication
+      Dream of Californication
+      Dream of Californication
+      Dream of Californication
+      Marry me, girl, be my fairy to the world, be my very own constellation
+      A teenage bride with a baby inside getting high on information
+      And buy me a star on the boulevard, it's Californication
+      Space may be the final frontier, but it's made in a Hollywood basement
+      And Cobain, can you hear the spheres singing songs off Station To Station?
+      And Alderaan's not far away, it's Californication
+      Born and raised by those who praise control of population
+      Well, everybody's been there and I don't mean on vacation
+      First born unicorn
+      Hardcore soft porn
+      Dream of Californication
+      Dream of Californication
+      Dream of Californication
+      Dream of Californication
+      Destruction leads to a very rough road, but it also breeds creation
+      And earthquakes are to a girl's guitar, they're just another good vibration
+      And tidal waves couldn't save the world from Californication
+      Pay your surgeon very well to break the spell of aging
+      Sicker than the rest, there is no test, but this is what you're craving
+      First born unicorn
+      Hardcore soft porn
+      Dream of Californication
+      Dream of Californication
+      Dream of Californication
+      Dream of Californication`,
+    },
+    {
+        src: "tracks/track-2.mp3",
+        albumArt: "images/album2.jpg",
+        trackName: "Lose Yourself",
+        lyrics: `[Intro]
+      Look... If you had... one shot... or one opportunity...
+      To seize everything you ever wanted... one moment...
+      Would you capture it? Or just let it slip?
+      Yo
+
+      [Verse 1]
+      His palms are sweaty, knees weak, arms are heavy
+      There's vomit on his sweater already, mom's spaghetti
+      He's nervous, but on the surface he looks calm and ready
+      to drops bombs, but he keeps on forgetting
+      what he wrote down, the whole crowd goes so loud
+      He opens his mouth but the words won't come out
+      He's chokin, how? Everybody's jokin now
+      The clock's run out, time's up, over - BLAOW!
+      Snap back to reality, OHH! there goes gravity
+      OHH! there goes Rabbit, he choked
+      He's so mad, but he won't
+      Give up that easy nope, he won't have it
+      He knows, his whole back's to these ropes
+      It don't matter, he's dope
+      He knows that, but he's broke
+      He's so sad that he knows
+      when he goes back to this mobile home, that's when it's
+      back to the lab again, yo, this whole rap shift
+      He better go capture this moment and hope it don't pass him
+
+      [Chorus]
+      You better - lose yourself in the music, the moment
+      You own it, you better never let it go (go)
+      You only get one shot, do not miss your chance to blow
+      This opportunity comes once in a lifetime
+      You better - lose yourself in the music, the moment
+      You own it, you better never let it go (go)
+      You only get one shot, do not miss your chance to blow
+      This opportunity comes once in a lifetime
+      You better..
+
+      [Verse 2]
+      Soul's escaping, through this hole that is gaping
+      This world is mine for the taking
+      Make me king, as we move toward a, new world order
+      A normal life is boring; but superstardom's
+      close to post-mortem, it only grows harder
+      Homie grows hotter, he blows it's all over
+      These hoes is all on him, coast to coast shows
+      He's known as the Globetrotter
+      Lonely roads, God only knows
+      He's grown farther from home, he's no father
+      He goes home and barely knows his own daughter
+      But hold your nose cause here goes the cold water
+      These hoes don't want him no mo', he's cold product
+      They moved on to the next schmoe who flows
+      He nose-dove and sold nada, and so the soap opera
+      is told, it unfolds, I suppose it's old partner
+      But the beat goes on da-da-dum da-dum da-dah
+
+      [Chorus]
+
+      [Verse 3]
+      No more games, Imma change what you call rage
+      Tear this motherfuckin roof off like two dogs caged
+      I was playin in the beginning, the mood all changed
+      I've been chewed up and spit out and booed off stage
+      But I kept rhymin and stepped right in the next cypher
+      Best believe somebody's payin the pied piper
+      All the pain inside amplified by the
+      fact that I can't get by with my nine to
+      five and I can't provide the right type of
+      life for my family, cause man, these God damn
+      food stamps don't buy diapers, and there's no movie
+      There's no Mekhi Phifer, this is my life
+      And these times are so hard, and it's gettin even harder
+      Tryin to feed and water my seed plus, teeter-totter
+      Caught up between bein a father and a pre-madonna
+      Baby momma drama screamin on her too much for me to wanna
+      stay in one spot, another day of monotony
+      has gotten me to the point, I'm like a snail I've got
+      to formulate a plot, or end up in jail or shot
+      Success is my only motherfuckin option, failure's not
+      Mom I love you but this trailer's got to go
+      I cannot grow old in Salem's Lot
+      So here I go it's my shot, feet fail me not
+      This may be the only opportunity that I got
+
+      [Chorus]
+
+      [Outro]
+      You can do anything you set your mind to, man.
+      `,
+    },
+    {
+        src: "tracks/track-3.mp3",
+        albumArt: "images/album3.jpg",
+        trackName: "The Real Slim Shady",
+    },
+    {
+        src: "tracks/track-4.mp3",
+        albumArt: "images/album4.jpg",
+        trackName: "Yellow",
+    },
+    {
+        src: "tracks/track-5.mp3",
+        albumArt: "images/album5.png",
+        trackName: "A Sky Full of Stars",
+    },
+    {
+        src: "tracks/track-6.mp3",
+        albumArt: "images/album6.jpg",
+        trackName: "Still DR. DRE",
+    },
+    {
+        src: "tracks/track-7.mp3",
+        albumArt: "images/album7.png",
+        trackName: "Rap God",
+    },
+    {
+        src: "tracks/track-8.mp3",
+        albumArt: "images/album8.jpg",
+        trackName: "Radioactive",
+    },
+    {
+        src: "tracks/track-9.mp3",
+        albumArt: "images/album9.jpg",
+        trackName: "Ransom",
+    },
+    {
+        src: "tracks/track-10.mp3",
+        albumArt: "images/album10.png",
+        trackName: "Beat it",
+    },
+    {
+        src: "tracks/track-11.mp3",
+        albumArt: "images/album11.jpg",
+        trackName: "Snow hey oh",
+    },
+    {
+        src: "tracks/track-12.mp3",
+        albumArt: "images/album12.jpg",
+        trackName: "By the Way",
+    },
+    {
+        src: "tracks/track-13.mp3",
+        albumArt: "images/album13.jpg",
+        trackName: "Shape of my Heart",
+    },
+    {
+        src: "tracks/track-14.mp3",
+        albumArt: "images/album14.png",
+        trackName: "Blinding lights",
+    },
+    {
+        src: "tracks/track-15.mp3",
+        albumArt: "images/album15.jpg",
+        trackName: "Diamonds",
+    },
+    {
+        src: "tracks/track-16.mp3",
+        albumArt: "images/album16.jpg",
+        trackName: "Counting stars",
+    },
+    {
+        src: "tracks/track-17.mp3",
+        albumArt: "images/album17.jpg",
+        trackName: "Wonderwall",
+    },
+    {
+        src: "tracks/track-18.mp3",
+        albumArt: "images/album17.jpg",
+        trackName: "Don't look back in anger",
+    },
+    {
+        src: "tracks/track-19.mp3",
+        albumArt: "images/album19.jpg",
+        trackName: "One more light",
+    },
+    {
+        src: "tracks/track-20.mp3",
+        albumArt: "images/album20.jpg",
+        trackName: "Love me again",
+    },
+    {
+        src: "tracks/track-21.mp3",
+        albumArt: "images/album21.jpg",
+        trackName: "Shape of you",
+    },
+    {
+        src: "tracks/track-22.mp3",
+        albumArt: "images/album22.jpg",
+        trackName: "Let me down slowly",
+    },
+    {
+        src: "tracks/track-23.mp3",
+        albumArt: "images/album23.jpg",
+        trackName: "Mood",
+    },
+    {
+        src: "tracks/track-24.mp3",
+        albumArt: "images/album25.jpg",
+        trackName: "Crazy Aerosmith",
+    },
+    {
+        src: "tracks/track-25.mp3",
+        albumArt: "images/album27.jpg",
+        trackName: "Every Breath You Take",
+    },
+    {
+        src: "tracks/track-26.mp3",
+        albumArt: "images/album30.jpg",
+        trackName: "Waka Waka",
+    },
+    {
+        src: "tracks/track-27.mp3",
+        albumArt: "images/album12.jpg",
+        trackName: "Can't stop",
+    },
+    {
+        src: "tracks/track-28.mp3",
+        albumArt: "images/album31.jpg",
+        trackName: "Live Forever",
+    },
+    {
+        src: "tracks/track-29.mp3",
+        albumArt: "images/album28.jpg",
+        trackName: "Payphone feat Wiz Kalifa",
+    },
+    {
+        src: "tracks/track-30.mp3",
+        albumArt: "images/album26.jpg",
+        trackName: "Sugar Maroon 5",
+    },
+    {
+        src: "tracks/track-31.mp3",
+        albumArt: "images/album24.jpg",
+        trackName: "What I,ve Done",
+    },
+    {
+        src: "tracks/track-32.mp3",
+        albumArt: "images/album32.jpg",
+        trackName: "Emptiness Machine",
+    },
+    {
+        src: "tracks/track-33.mp3",
+        albumArt: "images/album29.jpg",
+        trackName: "New Devide",
+    },
+];
